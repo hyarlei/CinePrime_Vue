47f3574580b3295a1143e08ab42d8a7b7bbb8997
Victor Cavalcante <cavalcantevictor2003@gmail.com>
2024-03-26 21:57:37 -0300

    trazendo os componentes

---
+++ frontend/src/components/Button/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <button :class="props.variant" @click="props.onClick" :type="props.type" :style="props.style">
+    {{ props.text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: String,
+    onClick: Function,
+    type: String,
+    style: Object,
+    text: String
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos opcionais podem ser adicionados aqui */
+</style>
---
+++ frontend/src/components/PageContainer/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="pageContainer">
+    <slot name="navbar"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageContainer'
+};
+</script>
+
+<style scoped>
+/* Estilos opcionais podem ser adicionados aqui */
+.pageContainer {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+</style>
