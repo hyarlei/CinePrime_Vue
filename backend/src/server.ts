08eedc7171ece88b15c9443b4a7c2b1315bc4440
unknown <hyarleysf@gmail.com>
2024-09-24 10:37:31 -0300

    Production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,8 +5,7 @@ import { routes } from './routes/routes';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3333;
-
+const PORT = process.env.VUE_BACKEND_PORT || 3333;
 
 app.use(cors({
 	origin: 'https://cine-prime-vue.vercel.app'  // Substitua pela URL real do frontend
@@ -16,8 +15,8 @@ app.use(express.json());
 
 app.use(routes);
 try {
-    app.listen(port, () => {
-        console.log(`Funcionando em http://localhost:${port}`);
+    app.listen(PORT, () => {
+        console.log(`Funcionando em http://localhost:${PORT}`);
     });
 } catch (error) {
     console.log(error);
